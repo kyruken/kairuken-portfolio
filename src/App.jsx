717ce9f0175e0991430e2cf85a73bcc6d624e189
Avery Kyle Arenas <kyruken@gmail.com>
2022-08-31 17:06:42 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import Header from "./components/Header.jsx"
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Technologies</li>
+            </ul>
+        </nav>
+    )
+}
