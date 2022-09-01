9c8c0af08b144821e36628d08c739914f91144fb
Avery Kyle Arenas <kyruken@gmail.com>
2022-08-31 18:08:11 -0700

    Create navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import React from "react"
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 export default function Header() {
     return (
-        <nav>
-            <ul>
+        <nav className="navbar-container">
+            <ul className="navbar-list-1-2">
                 <li>Home</li>
                 <li>Projects</li>
                 <li>Technologies</li>
             </ul>
+
+            <ul className="navbar-list-2-2">
+                <li><a href="google.com" target="_blank"><LinkedInIcon sx={{ fontSize: "3rem" }} /></a></li>
+                <a href="roblox.com" target="_blank"><GitHubIcon sx={{ fontSize: "3rem" }} /></a>
+            </ul>
+
         </nav>
     )
 }
